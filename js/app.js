86742d8e187192184f4bea271349d0cf36f23366
stefanom13 <stefanomancuso13@gmail.com>
2022-02-23 16:23:08 +0100

    generati 5 numeri

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 // generare 5 numeri casuali  
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+const min = 1;
+const max = 90;
+const numeridaIndovinare = [];
+
+
+
 
 // dopo 30 secondi chiedere all utente 5 numeri
 
